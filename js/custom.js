668af43671b2564b623226ef0c332d3747ee850e
Theanayne <anayne.zky@gmail.com>
2021-07-25 20:26:44 +0200

    first commit

---
+++ js/custom.js
@@ -0,0 +1,64 @@
+(function ($) {
+  "use strict";
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+          } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+          }
+    });
+
+
+    $('.owl-carousel').owlCarousel({
+      animateOut: 'fadeOut',
+      items:1,
+      loop:true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      smartSpeed: 1000,
+    })
+
+
+    $.stellar({
+      horizontalScrolling: false,
+    }); 
+
+
+    $('.image-popup').magnificPopup({
+        type: 'image',
+        removalDelay: 300,
+        mainClass: 'mfp-with-zoom',
+        gallery:{
+          enabled:true
+        },
+        zoom: {
+        enabled: true,
+
+        duration: 300,
+        easing: 'ease-in-out',
+
+        opener: function(openerElement) {
+        return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+      }
+    });  
+
+
+    $(function() {
+      $('.custom-navbar a, #home a').on('click', function(event) {
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+            event.preventDefault();
+      });
+    });  
+
+
+    new WOW({ mobile: false }).init();
+
+})(jQuery);
